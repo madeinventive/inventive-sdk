e36eb1fae215d2d23ce2d442ca37dd8470937224
pinyenchen <pin-yen@madeinventive.com>
2024-01-25 07:29:26 -0800

    feat: embed auth scope token related types

--- src/types/AuthorizedUrlInfo.ts
+++ src/types/AuthorizedUrlInfo.ts
@@ -3,4 +3,6 @@ import { TokensData } from './TokensData';
 export interface AuthorizedUrlInfo {
   url: string;
   tokens?: TokensData;
+  scopeToken?: string;
+  [k: string]: unknown; // allowing for any other properties
 }
